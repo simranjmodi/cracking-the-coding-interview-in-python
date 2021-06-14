c6a88c0800f4313667a247acecb5ff96b98077e9
Simran Modi <smod0001@student.monash.edu>
2021-06-14 20:06:26 +1000

    exercise 1.4 solution 2

--- chapter-01/exercise-1.4.2.py
+++ chapter-01/exercise-1.4.2.py
@@ -6,7 +6,13 @@ a palindrome. A palindrome is a word or phrase that is the same
 forwards and backwards. A permutation is a rearrangement of letters.
 The palindrome does not need to be limited to just dictionary words.
 
-Solution 2: Using single integer as bit vector to keep track of letters
+Solution 2: Using bit vector to keep track of letters
 """
 
-# TODO
+def can_permute_palindrome(s: str) -> bool:
+    result = 0
+    for c in s:
+        result ^= 1 << ord(c)
+
+    return result & (result - 1) == 0
+
