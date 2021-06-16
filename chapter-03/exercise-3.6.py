9a4c95cb68950d61bd315d624030b3edae5c492f
Simran Modi <smod0001@student.monash.edu>
2021-06-16 19:25:14 +1000

    exercise 3.6

--- chapter-03/exercise-3.6.py
+++ chapter-03/exercise-3.6.py
@@ -9,3 +9,68 @@ that type). They cannot select which specific animal they would like. Create
 the data structures to maintain this system and implement operations such as
 enqueue, dequeue_any, dequeue_dog and dequeue_cat.
 """
+
+from collections import deque
+
+class Dog:
+    def __init__(self,n):
+        self.order = -1
+        self.name = n
+
+    def set_order(self,ord):
+        self.order = ord
+
+    def get_order(self):
+        return self.order
+
+    def is_older_than(self,a):
+        return self.order < a.get_order()
+
+class Cat:
+    def __init__(self, n):
+        self.order = -1
+        self.name = n
+
+    def set_order(self, ord):
+        self.order = ord
+
+    def get_order(self):
+        return self.order
+
+    def is_older_than(self, a):
+        return self.order < a.get_order()
+
+class AnimalQueue:
+    def __init__(self):
+        self.dogs = deque()
+        self.cats = deque()
+        self.order = 0
+
+    def enque(self, a):
+        a.set_order(self.order)
+        self.order += 1
+
+        if type(a) == Dog:
+            self.dogs.append(a)
+        elif type(a) == Cat:
+            self.cats.append(a)
+
+    def dequeue_any(self):
+        if len(self.dogs) == 0:
+            return self.dequeue_cats()
+        elif len(self.cats) == 0:
+            return self.dequeue_dogs()
+
+        dog = self.dogs[0]
+        cat = self.cats[0]
+
+        if dog.is_older_than(cat):
+            return self.dequeue_dogs()
+        else:
+            return self.dequeue_cats()
+
+    def dequeue_dogs(self):
+        return self.dogs.popleft()
+
+    def dequeue_cats(self):
+        return self.cats.popleft()
