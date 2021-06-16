c8ab413a3d5d25a09c9a1765a0364973a997fad8
Simran Modi <smod0001@student.monash.edu>
2021-06-16 18:29:48 +1000

    exercise 3.5

--- chapter-03/exercise-3.5.py
+++ chapter-03/exercise-3.5.py
@@ -6,3 +6,18 @@ You can use an additional temporary stack, but you may not copy the elements
 into any other data structure (such as an array). The stack supports the
 following operations: push, pop, peek, and is_empty.
 """
+
+from collections import deque
+
+def sort(stack):
+    temp_stack = deque()
+    while len(stack) != 0:
+        tmp = stack.pop()
+        while len(temp_stack) != 0 and temp_stack[-1] > tmp:
+            stack.append(temp_stack.pop())
+
+        temp_stack.append(tmp)
+
+    while len(temp_stack) != 0:
+        stack.append(temp_stack.pop())
+    return stack
