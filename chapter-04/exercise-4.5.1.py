3f4231e650656df69e10ead5f97c0cf946a35911
Simran Modi <smod0001@student.monash.edu>
2021-06-23 19:10:45 +1000

    exercise 4.5.1

---
+++ chapter-04/exercise-4.5.1.py
@@ -0,0 +1,37 @@
+"""
+4.5 Validate BST
+
+Implement a function to check if a binary tree is a binary search tree
+
+Solution 1: In Order Traversal (Assuming no duplicate elements)
+"""
+
+last_printed = None
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def check_BST(n):
+    global last_printed
+
+    if n == None:
+        return True
+
+    if not check_BST(n.left):
+        return False
+
+    if last_printed is not None and n.data <= last_printed:
+        return False
+
+    last_printed = n.data
+
+    if not check_BST(n.right):
+        return False
+
+    return True
+
+
+
