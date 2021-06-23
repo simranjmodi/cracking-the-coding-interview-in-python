498f9c168c1b7afc597b95bf8100cc054c488e7e
Simran Modi <smod0001@student.monash.edu>
2021-06-23 18:49:29 +1000

    todo

--- chapter-04/exercise-4.3.py
+++ chapter-04/exercise-4.3.py
@@ -5,3 +5,5 @@ Given a binary tree, design an algorithm which creates a linked list of
 all the nodes at each depth (e.g. if you have a tree with depth D, you
 will have D linked lists)
 """
+
+# TODO
