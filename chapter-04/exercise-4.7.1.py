6a8ca5257609e5f26b01de13bb2ed9afe0f8dde1
Simran Modi <smod0001@student.monash.edu>
2021-06-24 14:13:31 +1000

    .

--- chapter-04/exercise-4.7.1.py
+++ chapter-04/exercise-4.7.1.py
@@ -6,6 +6,8 @@ pairs of projects, where the second project is dependent on the first project.
 All of a project's dependencies must be built before the project is. Find a
 build order that will allow the projects to be built. If there is no valid
 build order, return an error.
+
+Solution 1: Topological Sort
 """
 
 class Graph():
---
+++ chapter-04/exercise-4.7.2.py
@@ -0,0 +1,12 @@
+"""
+4.7 Build Order
+
+You are given a list of projects and list of dependencies (which is a list of
+pairs of projects, where the second project is dependent on the first project.
+All of a project's dependencies must be built before the project is. Find a
+build order that will allow the projects to be built. If there is no valid
+build order, return an error.
+
+Solution 2:
+"""
+
