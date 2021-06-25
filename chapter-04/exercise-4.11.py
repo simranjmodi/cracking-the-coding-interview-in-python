025b3238a1030c870cc193cf79921837ecc138ed
Simran Modi <smod0001@student.monash.edu>
2021-06-25 18:11:10 +1000

    qn info

---
+++ chapter-04/exercise-4.11.py
@@ -0,0 +1,9 @@
+"""
+4.11 Random Node
+
+You are implementing a binary search tree class from scratch, which, in addition to
+insert, find, and delete, has a method get_random_node() which returns a random node
+from the tree. All nodes should be equally likely to be chosen. Design and implement
+an algorithm for get_random_node(), and explain how you would implement the rest of
+the methods.
+"""
