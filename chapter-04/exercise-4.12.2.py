437aea0a2b425bad31b29c232cd90f5e88eb5ad5
Simran Modi <smod0001@student.monash.edu>
2021-07-01 18:26:53 +1000

    .

--- chapter-04/exercise-4.12.2.py
+++ chapter-04/exercise-4.12.2.py
@@ -6,13 +6,6 @@ might be positive or negative). Design an algorithm to count the number of paths
 that sum to a given value. The path does not need to start or end at the root or
 a leaf, but it must go downwards (travelling only from parent nodes to child nodes)
 
-Solution 1: Optimized
+Solution 2:
 """
 
-class Node:
-    def __init__(self, d):
-        self.data = d
-        self.right = None
-        self.left = None
-
-# TODO
