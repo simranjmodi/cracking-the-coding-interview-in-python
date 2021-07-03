f2bd20db8abe3be1c0810bb9e9696258bd5917c2
Simran Modi <smod0001@student.monash.edu>
2021-07-03 20:10:31 +1000

    comments

--- chapter-05/exercise-5.1.py
+++ chapter-05/exercise-5.1.py
@@ -8,14 +8,23 @@ all of M.
 """
 
 
-def insertion(n, m, i, j):
+def update_bits(n, m, i, j):
     clear_mask = -1 << (j + 1)
     capture_mask = (1 << i) - 1
+
+    # Capturing bits from i-1 to 0
     captured_bits = n & capture_mask
 
+    # Clearing bits from j to 0
     n &= clear_mask
+
+    # Shifting m to align with n
     m = m << i
+
+    # Insert m into n
     n |= m
+
+    # Insert captured bits
     n |= captured_bits
 
     return n
