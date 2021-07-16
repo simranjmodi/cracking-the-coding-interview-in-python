d5ce0bdf35e7b4da8221dc0fa0090f775b9afb96
Simran Modi <smod0001@student.monash.edu>
2021-07-16 11:45:52 +1000

    10.8

--- chapter-10/exercise-10.8.py
+++ chapter-10/exercise-10.8.py
@@ -7,4 +7,27 @@ is. With only 4 kilobytes of memory available, how would you print all
 duplicate elements in the array?
 """
 
-# TODO
+class BitArray:
+    def __init__(self, n):
+        self.arr = [0] * ((n >> 5) + 1)
+
+    def get(self, pos):
+        self.index = pos >> 5
+        self.bitNo = pos & 0x1F
+        return (self.arr[self.index] &
+                (1 << self.bitNo)) != 0
+
+    def set(self, pos):
+        self.index = pos >> 5
+        self.bitNo = pos & 0x1F
+        self.arr[self.index] |= (1 << self.bitNo)
+
+def check_duplicates(arr):
+    ba = BitArray(32000)
+
+    for i in range(len(arr)):
+        num = arr[i]
+        if ba.get(num):
+            print(num)
+        else:
+            ba.set(num)
